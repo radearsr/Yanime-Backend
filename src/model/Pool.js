d2a426ab3039ca060dd1d0af9732d183bf760f57
radearsr <radea.surya1@gmail.com>
2022-06-25 20:48:48 +0700

    add new configuration for production db

--- src/model/Pool.js
+++ src/model/Pool.js
@@ -1,10 +1,29 @@
 import mysql, { createPool } from "mysql";
 
+let config;
+
+if (process.env.NODE_ENV === "production") {
+  config = {
+    host: "sql3.freemysqlhosting.net",
+    database: "sql3502084",
+    user: "sql3502084",
+    password: "SdIbNLGg6q",
+  };
+} else {
+  config = {
+    host: "localhost",
+    database: "yanime",
+    user: "root",
+    password: "",
+  };
+}
+
 const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "yanime",
+  connectionLimit: 1000,
+  connectTimeout: 60 * 60 * 1000,
+  acquireTimeout: 60 * 60 * 1000,
+  timeout: 60 * 60 * 1000,
+  ...config,
 });
 
 export default mysql;
