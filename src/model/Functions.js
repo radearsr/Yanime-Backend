bf6970578234fb3921d32841bb8e6aadeec4a0a2
radearsr <radea.surya1@gmail.com>
2022-06-29 17:33:35 +0700

    add console log to log mysql format

--- src/model/Functions.js
+++ src/model/Functions.js
@@ -1,4 +1,4 @@
-import { pool } from "./Pool.js";
+import mysql, { pool } from "./Pool.js";
 
 const connectDB = () => (new Promise((resolve, reject) => {
   pool.getConnection((error, conn) => {
@@ -8,6 +8,7 @@ const connectDB = () => (new Promise((resolve, reject) => {
 }));
 
 const queryDB = (connection, sqlString, escapeStrValue) => (new Promise((resolve, reject) => {
+  console.log(mysql.format(sqlString, escapeStrValue));
   connection.query(sqlString, escapeStrValue, (error, result) => {
     if (error) reject(error);
     connection.release();
